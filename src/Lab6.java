45ab9f8b811b209205c907fb673d8af11b46b56e
arefin2022 <arefin12@gmail.com>
2022-03-13 12:22:21 -0400

    addition feature

--- src/Lab6.java
+++ src/Lab6.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Lab6 {
 
     public static void main(String[] args) {
+        double operand1, operand2, result;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter first operand: ");
+        operand1 = scanner.nextDouble();
+        System.out.print("Enter second operand: ");
+        operand2 = scanner.nextDouble();
 
+        result = operand1 + operand2;
+        System.out.println("The result of the operation is "+ result);
 
     }
 }
